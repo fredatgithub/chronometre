5bb9df2ae892f2923b7a4808d99df34bcd06e9e1
Freddy Juhel <domaine75@gmail.com>
2015-07-24 10:23:17 +0200

    Resharper code

--- Chronometre/AboutBox1.vb
+++ Chronometre/AboutBox1.vb
@@ -1,7 +1,6 @@
 ﻿Public NotInheritable Class AboutBox1
 
     Private Sub AboutBox1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ' Définissez le titre du formulaire.
         Dim applicationTitle As String
         If My.Application.Info.Title <> "" Then
             applicationTitle = My.Application.Info.Title
@@ -9,9 +8,6 @@
             applicationTitle = IO.Path.GetFileNameWithoutExtension(My.Application.Info.AssemblyName)
         End If
         Text = String.Format("À propos de {0}", applicationTitle)
-        ' Initialisez tout le texte affiché dans la boîte de dialogue À propos de.
-        ' TODO: personnalisez les informations d'assembly de l'application dans le volet "Application" de la 
-        '    boîte de dialogue Propriétés du projet (sous le menu "Projet").
         LabelProductName.Text = My.Application.Info.ProductName
         LabelVersion.Text = String.Format("Version {0}", My.Application.Info.Version.ToString)
         LabelCopyright.Text = My.Application.Info.Copyright
