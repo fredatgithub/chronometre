03e69e00a29ffcf231f8db5f3bd77ead1b269ff1
fred <domaine75@gmail.com>
2024-03-13 00:18:09 +0100

    update license

--- Chronometre/FormMain.vb
+++ Chronometre/FormMain.vb
@@ -1,6 +1,6 @@
 ﻿'
 'The MIT License(MIT)
-'Copyright(c) 2021 Freddy Juhel
+'Copyright(c) 2021-2024 Freddy Juhel
 'Permission is hereby granted, free of charge, to any person obtaining a copy
 'of this software and associated documentation files (the "Software"), to deal
 'in the Software without restriction, including without limitation the rights
